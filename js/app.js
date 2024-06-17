fdf632e7dc1ad8ad742f4a9d459b71e72ba78ddf
Aaron <aaronrosas0409@gmail.com>
2024-06-17 12:34:25 -0600

    Se modifica archivo de javascript, html y css

--- js/app.js
+++ js/app.js
@@ -1,22 +1,30 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+const $n = document.querySelector('#name');
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) {
+    try {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const data = await response.json();
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
+} catch (err) {
+    handleError(err);
+    }
 }
-
+ 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
   n.textContent = `Algo salió mal: ${err}`
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski')(handleError);
